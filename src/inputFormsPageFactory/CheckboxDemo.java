ddbccb300cc733ce1d9b7857885fa85ad7882e00
Norah Lovelace <call1800norah@gmail.com>
2021-08-17 16:55:31 -0500

    completed unittest methods

--- src/inputFormsPageFactory/CheckboxDemo.java
+++ src/inputFormsPageFactory/CheckboxDemo.java
@@ -3,6 +3,8 @@ package inputFormsPageFactory;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 /**
  * Created by norah on 7/22/2021.
  */
@@ -20,5 +22,24 @@ public class CheckboxDemo {
 
     @FindBy(xpath = "//div[@id='txtAge']")
     public  WebElement CheckboxCheckedMessage;
+
+    //Multiple checkbox Demo
+    @FindBy(xpath = "//div[@class='panel-heading' and text()='Multiple Checkbox Demo']")
+    public  WebElement MultiCheckboxHeader;
+
+    @FindBy(xpath = "//div[@class='panel-body']//p[text()='Check the below points before automating']")
+    public  WebElement MultiCheckboxMessage;
+
+    @FindBy(xpath = "//div[@class='col-md-6 text-left']//ul//li")
+    public List<WebElement> MultiCheckboxMessages;
+
+    @FindBy(xpath = "//*[@id='easycont']/div/div[2]/div[2]/div[2]/div/label")
+    public List<WebElement> CheckboxOptions;
+
+    @FindBy(xpath = "//input[@id='check1' and @value='Check All']")
+    public WebElement CheckAllButton;
+
+    @FindBy(xpath = "//input[@id='check1' and @value='Uncheck All']")
+    public WebElement UnCheckAllButton;
 
 }
--- src/inputFormsTests/CheckboxDemoUnitTest.java
+++ src/inputFormsTests/CheckboxDemoUnitTest.java
@@ -1,7 +1,16 @@
 package inputFormsTests;
 
+import UnitTestBase.UnitTestBaseMethods;
+import inputFormsPageFactory.CheckboxDemo;
+
 /**
  * Created by norah on 7/22/2021.
  */
-public class CheckboxDemoUnitTest {
+public class CheckboxDemoUnitTest extends UnitTestBaseMethods{
+    CheckboxDemo checkboxDemo;
+    public void SingleCheckboxDemo(){
+
+    }
+
+
 }
