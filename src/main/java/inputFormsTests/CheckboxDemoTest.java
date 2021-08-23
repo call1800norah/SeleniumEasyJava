dd9712b807405767ccceb9160ad31c8fe34471ea
Norah Lovelace <call1800norah@gmail.com>
2021-08-22 20:09:19 -0500

    Simple Form Demo test passed

---
+++ src/main/java/inputFormsTests/CheckboxDemoTest.java
@@ -0,0 +1,16 @@
+package inputFormsTests;
+
+import TestBase.TestBaseMethods;
+import inputFormsPageFactory.CheckboxDemo;
+
+/**
+ * Created by norah on 7/22/2021.
+ */
+public class CheckboxDemoTest extends TestBaseMethods {
+    CheckboxDemo checkboxDemo;
+    public void SingleCheckboxDemo(){
+
+    }
+
+
+}
